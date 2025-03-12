76ee7746cb2a93c5d2dc2669fce90ec145607858
vedantika-04 <vedantikasharmaindia@gmail.com>
2025-03-12 10:46:31 +0530

    Testing webhook

--- main/hello.cpp
+++ main/hello.cpp
@@ -1,10 +1,12 @@
-//File: hello.cpp
+// File: hello.cpp
 #include <iostream>
 using namespace std;
 
-int main(void) {
+int main(void)
+{
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
+  cout << "Hello, Jenkins Webhook!";
   return 0;
 }
